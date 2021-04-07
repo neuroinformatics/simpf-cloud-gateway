d03ae96dc3628c455d61ba14af7e9dc20c8ec72f
Yoshihiro OKUMURA <orrisroot@gmail.com>
2021-04-08 03:22:41 +0900

    fixed bug to set controllers.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DownloadController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
+Route::get('/download/{sid}/{fname}', DownloadController::class);
